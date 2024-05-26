ccfa724acb0ae9797f090a38814dcbdcd4c4938d
Martin Jurkovic <martin.jurkovic19@gmail.com>
2024-05-26 17:20:46 +0200

    Add duckdb init files

---
+++ src/dask_duckdb/duckdb_setup.sql
@@ -0,0 +1,3 @@
+CREATE TABLE nyc AS FROM read_parquet('./data/parquet/2014.parquet');
+-- INSERT INTO nyc FROM read_parquet('./data/parquet/2015.parquet');
+-- INSERT INTO nyc FROM read_parquet('./data/parquet/2016.parquet');
