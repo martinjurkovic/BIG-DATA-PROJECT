9b1f67f96040935552a3235ce29a70c1eedf6123
Martin Jurkovic <martin.jurkovic19@gmail.com>
2024-05-26 15:52:02 +0200

    Add CSV data download script

---
+++ data/CSV/nyt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl https://data.cityofnewyork.us/api/views/pvqr-7yc4/rows.csv -o 2024_april.csv
+#curl https://data.cityofnewyork.us/api/views/869v-vr48/rows.csv -o 2023.csv
+#curl https://data.cityofnewyork.us/api/views/7mxj-7a6y/rows.csv -o 2022.csv
+#curl https://data.cityofnewyork.us/api/views/pvqr-7yc4/rows.csv -o 2022.csv
+#curl https://data.cityofnewyork.us/api/views/kvfd-bves/rows.csv -o 2021.csv
+#curl https://data.cityofnewyork.us/api/views/p7t3-5i9s/rows.csv -o 2020.csv
+#curl https://data.cityofnewyork.us/api/views/faiq-9dfq/rows.csv -o 2019.csv
+#curl https://data.cityofnewyork.us/api/views/a5td-mswe/rows.csv -o 2018.csv
+#curl https://data.cityofnewyork.us/api/views/2bnn-yakx/rows.csv -o 2017.csv
+#curl https://data.cityofnewyork.us/api/views/kiv2-tbus/rows.csv -o 2016.csv
+#curl https://data.cityofnewyork.us/api/views/c284-tqph/rows.csv -o 2015.csv
+#curl https://data.cityofnewyork.us/api/views/jt7v-77mi/rows.csv -o 2014.csv
