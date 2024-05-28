7b6dd97beef3979c7aefbac71f7f7535d2b6c0c4
Martin Jurkovic <martin.jurkovic19@gmail.com>
2024-05-28 19:31:33 +0200

    Add fixed column names to T1 data format conversion

--- src/T1_data_format_conversion/csv_to_hdf5.py
+++ src/T1_data_format_conversion/csv_to_hdf5.py
@@ -7,6 +7,8 @@ cwd = Path.cwd()
 path = str(cwd).split("BIG-DATA-PROJECT")
 CSV_PATH = Path(path[0]) / "BIG-DATA-PROJECT/data/CSV/"
 
+columns = ['Summons Number', 'Plate ID', 'Registration State', 'Plate Type', 'Issue Date', 'Violation Code', 'Vehicle Body Type', 'Vehicle Make', 'Issuing Agency', 'Street Code1', 'Street Code2', 'Street Code3', 'Vehicle Expiration Date', 'Violation Location', 'Violation Precinct', 'Issuer Precinct', 'Issuer Code', 'Issuer Command', 'Issuer Squad', 'Violation Time', 'Time First Observed', 'Violation County', 'Violation In Front Of Or Opposite', 'House Number', 'Street Name', 'Intersecting Street', 'Date First Observed', 'Law Section', 'Sub Division', 'Violation Legal Code', 'Days Parking In Effect    ', 'From Hours In Effect', 'To Hours In Effect', 'Vehicle Color', 'Unregistered Vehicle?', 'Vehicle Year', 'Meter Number', 'Feet From Curb', 'Violation Post Code', 'Violation Description', 'No Standing or Stopping Violation', 'Hydrant Violation', 'Double Parking Violation']
+
 # create a directory to store the HDF5 files
 HDF5_PATH = Path(path[0]) / "BIG-DATA-PROJECT/data/HDF5/"
 if not os.path.exists(HDF5_PATH):
@@ -17,5 +19,5 @@ for file in os.listdir(CSV_PATH):
     if file.endswith('.csv'):
         print(f"Converting {file} to HDF5.")
         # read the csv file
-        df = pd.read_csv(CSV_PATH / file, low_memory=False)
+        df = pd.read_csv(CSV_PATH / file, low_memory=False, names = columns, header=0)
         df.to_hdf(HDF5_PATH / (file.split(".")[0] + ".h5"), key='df', mode='w')
--- src/T1_data_format_conversion/csv_to_parquet.py
+++ src/T1_data_format_conversion/csv_to_parquet.py
@@ -14,11 +14,19 @@ PARQUET_PATH = Path(path[0]) / "BIG-DATA-PROJECT/data/parquet/"
 if not os.path.exists(PARQUET_PATH):
     os.makedirs(PARQUET_PATH)
 
+columns = ['Summons Number', 'Plate ID', 'Registration State', 'Plate Type', 'Issue Date', 'Violation Code', 'Vehicle Body Type', 'Vehicle Make', 'Issuing Agency', 'Street Code1', 'Street Code2', 'Street Code3', 'Vehicle Expiration Date', 'Violation Location', 'Violation Precinct', 'Issuer Precinct', 'Issuer Code', 'Issuer Command', 'Issuer Squad', 'Violation Time', 'Time First Observed', 'Violation County', 'Violation In Front Of Or Opposite', 'House Number', 'Street Name', 'Intersecting Street', 'Date First Observed', 'Law Section', 'Sub Division', 'Violation Legal Code', 'Days Parking In Effect    ', 'From Hours In Effect', 'To Hours In Effect', 'Vehicle Color', 'Unregistered Vehicle?', 'Vehicle Year', 'Meter Number', 'Feet From Curb', 'Violation Post Code', 'Violation Description', 'No Standing or Stopping Violation', 'Hydrant Violation', 'Double Parking Violation']
+
 # loop over all the csv files in the directory
 for file in os.listdir(CSV_PATH):
     if file.endswith('.csv'):
+        parquet_file_path = PARQUET_PATH / (file.split(".")[0] + ".parquet")
+        if parquet_file_path.exists():
+            print(f"{parquet_file_path} already exists. Skipping conversion.")
+            continue
+                                            
+
         print(f"Converting {file} to parquet.")
         # read the csv file
-        df = pd.read_csv(CSV_PATH / file, low_memory=False)
+        df = pd.read_csv(CSV_PATH / file, low_memory=False, names = columns, header=0)
         table = pa.Table.from_pandas(df)
-        pq.write_table(table, PARQUET_PATH / (file.split(".")[0] + ".parquet"))
+        pq.write_table(table, parquet_file_path)
