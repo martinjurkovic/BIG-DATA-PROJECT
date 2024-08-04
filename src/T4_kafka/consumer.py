299c95abd7d0982b1e96dd2271d25fcd0db232f8
ValterH <valter.hudovernik@gmail.com>
2024-08-04 15:04:07 +0200

    lint kafka

--- src/T4_kafka/consumer.py
+++ src/T4_kafka/consumer.py
@@ -6,11 +6,13 @@ import pandas as pd
 from confluent_kafka import Consumer, OFFSET_BEGINNING
 import socket
 
-if __name__ == '__main__':
-    config = {'bootstrap.servers': "localhost:29092",
-                            'client.id': socket.gethostname(),
-                             'group.id': 'test_group', 
-                             'auto.offset.reset': 'earliest'}
+if __name__ == "__main__":
+    config = {
+        "bootstrap.servers": "localhost:29092",
+        "client.id": socket.gethostname(),
+        "group.id": "test_group",
+        "auto.offset.reset": "earliest",
+    }
     # # Create Consumer instance
     consumer = Consumer(config)
 
@@ -26,31 +28,33 @@ if __name__ == '__main__':
 
     # Poll for new messages from Kafka and print them.
     # create a dataframe indexed by date from 01/01/2024 to 12/31/2024 with columns for each NYC borough
-    data = pd.DataFrame(index=pd.date_range(start='2014-01-01', end='2024-12-31', freq='D'),
-                        columns=['BX', 'BK', 'MN', 'QS', 'SI', 'TOTAL'])
+    data = pd.DataFrame(
+        index=pd.date_range(start="2014-01-01", end="2024-12-31", freq="D"),
+        columns=["BX", "BK", "MN", "QS", "SI", "TOTAL"],
+    )
     borough_map = {
-        'BX': 'BX',
-        'BRONX': 'BX',
-        'BK': 'BK',
-        'BROOKLYN': 'BK',
-        'K': 'BK',
-        'KINGS': 'BK',
-        'MN': 'MN',
-        'MANHATTAN': 'MN',
-        'Q': 'QS',
-        'QS': 'QS',
-        'QN': 'QS',
-        'QNS': 'QS',
-        'QUEENS': 'QS',
-        'SI': 'SI',
-        'ST': 'SI',
-        'STATEN ISLAND': 'SI',
-        'NY': 'TOTAL',
-        '' : 'TOTAL',
-        'R': 'TOTAL',
+        "BX": "BX",
+        "BRONX": "BX",
+        "BK": "BK",
+        "BROOKLYN": "BK",
+        "K": "BK",
+        "KINGS": "BK",
+        "MN": "MN",
+        "MANHATTAN": "MN",
+        "Q": "QS",
+        "QS": "QS",
+        "QN": "QS",
+        "QNS": "QS",
+        "QUEENS": "QS",
+        "SI": "SI",
+        "ST": "SI",
+        "STATEN ISLAND": "SI",
+        "NY": "TOTAL",
+        "": "TOTAL",
+        "R": "TOTAL",
     }
 
-    data = data.fillna(0.)
+    data = data.fillna(0.0)
     try:
         while True:
             msg = consumer.poll(1.0)
@@ -63,18 +67,18 @@ if __name__ == '__main__':
                 print("ERROR: %s".format(msg.error()))
             else:
                 # Extract the (optional) key and value, and print.
-                msg_data = json.loads(msg.value().decode('utf-8'))
-                key = msg.key().decode('utf-8')
+                msg_data = json.loads(msg.value().decode("utf-8"))
+                key = msg.key().decode("utf-8")
                 boroug = borough_map[key.upper()]
-                #get only the date without the time
-                date = pd.to_datetime(msg_data['Issue Date'])
+                # get only the date without the time
+                date = pd.to_datetime(msg_data["Issue Date"])
                 try:
                     data.loc[date, boroug] += 1
                 except KeyError as e:
                     print(f"KeyError: {e}")
                     continue
-                if boroug != 'TOTAL':
-                    data.loc[date, 'TOTAL'] += 1
+                if boroug != "TOTAL":
+                    data.loc[date, "TOTAL"] += 1
                 print(f"{msg.topic()}: {boroug} - {date} - {data.loc[date, boroug]}")
     except KeyboardInterrupt:
         pass
--- src/T4_kafka/faust_worker.py
+++ src/T4_kafka/faust_worker.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import faust
 import numpy as np
 
+
 class WeatherData(faust.Record):
     WBANNO: str
     UTC_DATE: str
@@ -45,27 +46,35 @@ class HighestTemperature(faust.Record):
     station: str
     temperature: float
 
-app = faust.App('weather_app', broker='kafka://localhost:29092')
 
-weather_data_topic = app.topic('weather_data', key_type=str, value_type=WeatherData)
-outliers_topic = app.topic('outliers', key_type=str, value_type=OutlierData)
-hourly_data_topic = app.topic('hourly_temperature', key_type=str, value_type=HourlyData)
-highest_temperature_topic = app.topic('highest_temperature', key_type=str, value_type=HighestTemperature)
+app = faust.App("weather_app", broker="kafka://localhost:29092")
 
-hourly_temperature_table = app.Table('hourly_temperature', key_type=str, value_type=float, partitions=1, default=float)
-hourly_count_table       = app.Table('hourly_count', key_type=str, value_type=int, partitions=1, default=int)
+weather_data_topic = app.topic("weather_data", key_type=str, value_type=WeatherData)
+outliers_topic = app.topic("outliers", key_type=str, value_type=OutlierData)
+hourly_data_topic = app.topic("hourly_temperature", key_type=str, value_type=HourlyData)
+highest_temperature_topic = app.topic(
+    "highest_temperature", key_type=str, value_type=HighestTemperature
+)
+
+hourly_temperature_table = app.Table(
+    "hourly_temperature", key_type=str, value_type=float, partitions=1, default=float
+)
+hourly_count_table = app.Table(
+    "hourly_count", key_type=str, value_type=int, partitions=1, default=int
+)
 
 
-N = 36 # 3 hours
+N = 36  # 3 hours
 MAX_TEMP = 50
 reference_values = {
-    'Aleknagik_1_NNE': [],
-    'Bethel_87_WNW': [],
-    'Cordova_14_ESE': [],
+    "Aleknagik_1_NNE": [],
+    "Bethel_87_WNW": [],
+    "Cordova_14_ESE": [],
 }
 SMOOTHING_FACTOR = 0.1
 SCALE_STD = 1.25
 momentum = 0
+
 
 @app.agent(weather_data_topic)
 async def weather_data_stream(data):
@@ -88,7 +97,14 @@ async def weather_data_stream(data):
                 # dampen the momentum
                 momentum = SMOOTHING_FACTOR * momentum
                 # send the record to the outliers topic
-                await outliers_topic.send(key=key, value={'temp': np.round(air_temp, 2), 'mean': np.round(mean, 2), 'std': np.round(std, 2)})
+                await outliers_topic.send(
+                    key=key,
+                    value={
+                        "temp": np.round(air_temp, 2),
+                        "mean": np.round(mean, 2),
+                        "std": np.round(std, 2),
+                    },
+                )
                 continue
             # check if the record is an outlier
             # project the record back based on the momentum
@@ -101,7 +117,7 @@ async def weather_data_stream(data):
                 # print(f"Anomaly detected: {air_temp}, Z-Score: {z_score}, {z_score_proj}*")
                 # print(f"Temp: {air_temp} -> {projected}, Mean: {mean}, {adj_mean}*, Std: {std}, {adj_std}*")
                 # time.sleep(2)
-                
+
                 # some sudden shifts are possible
                 # if the z-score is less than 4.5, use the new record in mean calculation
                 if z_score < 4.5:
@@ -116,13 +132,22 @@ async def weather_data_stream(data):
                 # dampen the momentum
                 momentum = SMOOTHING_FACTOR * momentum
                 # send the record to the outliers topic
-                await outliers_topic.send(key=key, value={'temp': np.round(air_temp, 2), 'mean': np.round(mean, 2), 'std': np.round(std, 2)})
+                await outliers_topic.send(
+                    key=key,
+                    value={
+                        "temp": np.round(air_temp, 2),
+                        "mean": np.round(mean, 2),
+                        "std": np.round(std, 2),
+                    },
+                )
                 continue
             # update the momentum
-            momentum = SMOOTHING_FACTOR * momentum + (1-SMOOTHING_FACTOR) * (air_temp - reference_values[key][-1])
+            momentum = SMOOTHING_FACTOR * momentum + (1 - SMOOTHING_FACTOR) * (
+                air_temp - reference_values[key][-1]
+            )
             # print(f"{reference_values[key][-1]} -> {air_temp}, Momentum: {momentum}")
             # time.sleep(0.5)
-            
+
             # remove the oldest value
             reference_values[key].pop(0)
             reference_values[key].append(air_temp)
@@ -132,32 +157,51 @@ async def weather_data_stream(data):
         hourly_temperature_table[f"{key}", f"{hour}", date] += air_temp
         hourly_count_table[f"{key}", f"{hour}", date] += 1
 
+
 CURRENT_HOUR = 0
-CURRENT_DATE = datetime.strptime('20210101', '%Y%m%d')
+CURRENT_DATE = datetime.strptime("20210101", "%Y%m%d")
+
+
 @app.timer(interval=10)  # Timer to trigger hourly computation
 async def compute_hourly_temperature():
     global CURRENT_HOUR, CURRENT_DATE
 
     hourly_data = dict()
     hour = f"{CURRENT_HOUR}"
-    date = datetime.strftime(CURRENT_DATE, '%Y%m%d')
+    date = datetime.strftime(CURRENT_DATE, "%Y%m%d")
 
-    for station_name in ['Aleknagik_1_NNE', 'Bethel_87_WNW', 'Cordova_14_ESE']:
+    for station_name in ["Aleknagik_1_NNE", "Bethel_87_WNW", "Cordova_14_ESE"]:
         if (station_name, hour, date) in hourly_temperature_table:
             temperature_sum = hourly_temperature_table[station_name, hour, date]
             temperature_count = hourly_count_table[station_name, hour, date]
             # print(f"{station_name} {hour} {temperature_sum} {temperature_count}")
             if temperature_count > 0:
                 hourly_mean_temperature = temperature_sum / temperature_count
-                print(f"Hourly mean temperature for station {station_name} @ {date}-{hour}: {hourly_mean_temperature}")
-                await hourly_data_topic.send(key=station_name, value={'hour': hour, 'temperature': np.round(hourly_mean_temperature, 3)})
+                print(
+                    f"Hourly mean temperature for station {station_name} @ {date}-{hour}: {hourly_mean_temperature}"
+                )
+                await hourly_data_topic.send(
+                    key=station_name,
+                    value={
+                        "hour": hour,
+                        "temperature": np.round(hourly_mean_temperature, 3),
+                    },
+                )
                 hourly_data[station_name] = hourly_mean_temperature
-    
+
     if len(hourly_data):
         # get hottest station
         hottest_station = max(hourly_data, key=hourly_data.get)
-        print(f"Hottest station at hour {CURRENT_HOUR} is {hottest_station} with temperature {hourly_data[hottest_station]}")
-        await highest_temperature_topic.send(key=hour, value={'station': hottest_station, 'temperature': np.round(hourly_data[hottest_station], 4)})
+        print(
+            f"Hottest station at hour {CURRENT_HOUR} is {hottest_station} with temperature {hourly_data[hottest_station]}"
+        )
+        await highest_temperature_topic.send(
+            key=hour,
+            value={
+                "station": hottest_station,
+                "temperature": np.round(hourly_data[hottest_station], 4),
+            },
+        )
 
         CURRENT_HOUR += 1
         if CURRENT_HOUR == 24:
