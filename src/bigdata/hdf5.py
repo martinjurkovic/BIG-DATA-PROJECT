fd3ca9b13b699b3dc9a0036c0835b9b27ca8babe
ValterH <valter.hudovernik@gmail.com>
2024-08-08 11:32:45 +0200

    update hdf5 file saving

--- src/bigdata/hdf5.py
+++ src/bigdata/hdf5.py
@@ -1,5 +1,11 @@
+import unicodedata
+
 import numpy as np
 import h5py
+
+
+def clean_string(s):
+    return unicodedata.normalize("NFKD", str(s)).encode("ascii", "ignore").decode()
 
 
 def process_type(type):
@@ -16,6 +22,10 @@ def process_type(type):
 
 def save_to_hdf5(path, df):
     data_types = [(name, process_type(type)) for name, type in df.dtypes.items()]
+    for col, dtype in data_types:
+        if dtype == "S20":
+            df[col] = df[col].apply(clean_string)
+
     structured_array = np.core.records.fromarrays(
         df.values.T, names=",".join(df.columns), dtype=data_types
     )
