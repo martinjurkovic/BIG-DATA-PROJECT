f8c8e9b713a81f4bd49f04aacca3db620accfbca
ValterH <valter.hudovernik@gmail.com>
2024-08-09 13:51:55 +0200

    fix T3

--- src/T3_exploratory_data_analysis/exploratory_data_analysis.py
+++ src/T3_exploratory_data_analysis/exploratory_data_analysis.py
@@ -1,10 +1,12 @@
-# %%
-import dask.dataframe as dd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import time
 from pathlib import Path
 import argparse
+
+import pandas as pd
+import seaborn as sns
+import dask.dataframe as dd
+import matplotlib.pyplot as plt
+
 from bigdata.utils import read_files
 from bigdata.utils import run_with_memory_log
 from dask.distributed import LocalCluster, Client
@@ -57,43 +59,59 @@ def main():
             f.write(f"\nMean Time: {mean_time:.2f} seconds\n")
             f.write(f"Standard Deviation: {std_time:.2f} seconds\n")
 
+    # Read the data
+    ddf = read_files(
+        base_path,
+        file_format,
+        usecols=[
+            "Registration State",
+            "Vehicle Body Type",
+            "Vehicle Year",
+            "Violation Code",
+            "Feet From Curb",
+            "Issue Date",
+            "Violation Precinct",
+            "Issuing Agency",
+            "Vehicle Make",
+            "Violation Location",
+            "Violation Time",
+            "Violation Precinct",
+        ],
+        years=years,
+    ).persist()
+
     # %%
     # Distribution of Registration States
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Registration State"], years=years
-    ).compute()
+    df = ddf[["Registration State"]].compute()
     df_reg_state = df["Registration State"].value_counts()
     plt.figure(figsize=(12, 6))
     sns.countplot(data=df, x="Registration State", order=df_reg_state.index)
     plt.title("Distribution of Registration States")
     plt.xticks(rotation=90)
     plt.savefig(fig_dir / f"(distribution_registration_states_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Distribution of 'Vehicle Body Type'
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Vehicle Body Type"], years=years
-    ).compute()
-    df = df.reset_index(drop=True)
+    df = ddf[["Vehicle Body Type"]].compute().reset_index(drop=True)
     df_reg_state = df["Vehicle Body Type"].value_counts()
     plt.figure(figsize=(12, 6))
     sns.countplot(data=df, x="Vehicle Body Type", order=list(df_reg_state.index[:10]))
     plt.title("Distribution of Vehicle Body Type")
     plt.xticks(rotation=90)
     plt.savefig(fig_dir / f"(distribution_vehicle_body_type_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Distribution of Vehicle Year
     start_time = time.time()
-    df = read_files(base_path, file_format, usecols=["Vehicle Year"], years=years)
+    df = ddf[["Vehicle Year"]]
     df["Vehicle Year"] = dd.to_numeric(df["Vehicle Year"], errors="coerce")
     df["Vehicle Year"] = df["Vehicle Year"].fillna(0).astype(int)
     df = df[df["Vehicle Year"] != 0]
@@ -101,174 +119,150 @@ def main():
     sns.histplot(df["Vehicle Year"].compute(), bins=30)
     plt.title("Distribution of Vehicle Year")
     plt.savefig(fig_dir / f"(distribution_vehicle_year_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Correlation Matrix
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Vehicle Year", "Feet From Curb"], years=years
-    )
-    df = df.dropna()
-    df_corr = df.corr().compute()
+    df = ddf[["Vehicle Year", "Feet From Curb"]].compute().dropna()
+    df_corr = df.corr()
     plt.figure(figsize=(10, 8))
     sns.heatmap(df_corr, annot=True, cmap="coolwarm", fmt=".2f")
     plt.title("Correlation Matrix")
     plt.savefig(fig_dir / f"(correlation_matrix_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Top 10 Most Frequent Violations
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Violation Code"], years=years
-    ).compute()
-    df = df.reset_index(drop=True)
+    df = ddf[["Violation Code"]].compute().reset_index(drop=True)
     df_violation_code = df["Violation Code"].value_counts().head(10)
     plt.figure(figsize=(12, 6))
     sns.countplot(data=df, x="Violation Code", order=df_violation_code.index)
     plt.title("Top 10 Most Frequent Violations")
     plt.xticks(rotation=90)
     plt.savefig(fig_dir / f"(top_10_most_frequent_violations_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Violations Over Time
     start_time = time.time()
-    df = read_files(base_path, file_format, usecols=["Issue Date"], years=years)
-    df["Issue Date"] = dd.to_datetime(df["Issue Date"])
-    violations_over_time = df["Issue Date"].value_counts().compute().sort_index()
+    df = ddf[["Issue Date"]].compute()
+    df["Issue Date"] = pd.to_datetime(df["Issue Date"], errors="coerce")
+    df.dropna(inplace=True)
+    violations_over_time = df["Issue Date"].value_counts().sort_index()
     plt.figure(figsize=(12, 6))
     violations_over_time.plot()
     plt.title("Violations Over Time")
     plt.xlabel("Date")
     plt.ylabel("Number of Violations")
     plt.savefig(fig_dir / f"(violations_over_time_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Violations by Precinct
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Violation Precinct"], years=years
-    ).compute()
-    df = df.reset_index(drop=True)
+    df = ddf[["Violation Precinct"]].compute().reset_index(drop=True)
     df_precinct = df["Violation Precinct"].value_counts().head(10)
     plt.figure(figsize=(12, 6))
     sns.countplot(data=df, x="Violation Precinct", order=df_precinct.index)
     plt.title("Violations by Precinct")
     plt.xticks(rotation=90)
     plt.savefig(fig_dir / f"(violations_by_precinct_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Top 10 Vehicle Makes with Most Violations
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Vehicle Make"], years=years
-    ).compute()
-    df = df.reset_index(drop=True)
+    df = ddf[["Vehicle Make"]].compute().reset_index(drop=True)
     df_make = df["Vehicle Make"].value_counts().head(10)
     plt.figure(figsize=(12, 6))
     sns.countplot(data=df, x="Vehicle Make", order=df_make.index)
     plt.title("Top 10 Vehicle Makes with Most Violations")
     plt.xticks(rotation=90)
     plt.savefig(fig_dir / f"(top_10_vehicle_makes_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Violations by Issuing Agency
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Issuing Agency"], years=years
-    ).compute()
-    df = df.reset_index(drop=True)
+    df = ddf[["Issuing Agency"]].compute().reset_index(drop=True)
     df_agency = df["Issuing Agency"].value_counts()
     plt.figure(figsize=(12, 6))
     sns.countplot(data=df, x="Issuing Agency", order=df_agency.index)
     plt.title("Violations by Issuing Agency")
     plt.xticks(rotation=90)
     plt.savefig(fig_dir / f"(violations_by_issuing_agency_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Distribution of Violation Times
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Violation Time"], years=years
-    ).compute()
-    df = df.reset_index(drop=True)
+    df = ddf[["Violation Time"]].compute().reset_index(drop=True)
     plt.figure(figsize=(12, 6))
     sns.histplot(data=df, x="Violation Time", bins=24, kde=True)
     plt.title("Distribution of Violation Times")
     plt.savefig(fig_dir / f"(distribution_violation_times_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Heatmap of Violation Counts by Location and Precinct
     start_time = time.time()
-    df = read_files(
-        base_path,
-        file_format,
-        usecols=["Violation Precinct", "Violation Location"],
-        years=years,
-    )
-    violation_location_precinct = (
-        df.groupby(["Violation Precinct", "Violation Location"])
+    # df = ddf[["Violation Precinct", "Violation Location"]].compute()
+    df = (
+        ddf.groupby(["Violation Precinct", "Violation Location"])
         .size()
         .compute()
         .unstack(fill_value=0)
     )
     plt.figure(figsize=(12, 8))
-    sns.heatmap(violation_location_precinct, cmap="coolwarm")
+    sns.heatmap(df, cmap="coolwarm")
     plt.title("Heatmap of Violation Counts by Location and Precinct")
     plt.savefig(fig_dir / f"(heatmap_violation_counts_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Box Plot of Feet From Curb
     start_time = time.time()
-    df = read_files(
-        base_path, file_format, usecols=["Feet From Curb"], years=years
-    ).compute()
+    df = ddf[["Feet From Curb"]].compute()
     plt.figure(figsize=(12, 6))
     sns.boxplot(data=df, x="Feet From Curb")
     plt.title("Box Plot of Feet From Curb")
     plt.savefig(fig_dir / f"(box_plot_feet_from_curb_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
     # %%
     # Box Plot of Vehicle Year
     start_time = time.time()
-    df = read_files(base_path, file_format, usecols=["Vehicle Year"], years=years)
+    df = ddf[["Vehicle Year"]]
     df["Vehicle Year"] = dd.to_numeric(df["Vehicle Year"], errors="coerce")
     df["Vehicle Year"] = df["Vehicle Year"].fillna(0).astype(int)
-    df = df[df["Vehicle Year"] != 0]
+    df = df[df["Vehicle Year"] != 0].compute()
     plt.figure(figsize=(12, 6))
-    sns.boxplot(x=df["Vehicle Year"].compute())
+    sns.boxplot(x=df["Vehicle Year"])
     plt.title("Box Plot of Vehicle Year")
     plt.savefig(fig_dir / f"(box_plot_vehicle_year_dask)_{file_format}.png")
-    plt.close()
+
     end_time = time.time()
     plot_times.append(end_time - start_time)
 
